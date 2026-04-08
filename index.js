564108b16111dc33cf51508994aa65ab33bc81ad
Filipp <wolverinexdd@yandex.ru>
2026-04-08 19:35:39 +0400

    Lesson02

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+const title = 'Project';
+const screens = 'Простые, Сложные, Интерактивные';
+const screenPrice = 5;
+const rollback = 90;
+const fullPrice = 200000;
+const adaptive = true;
 
 
-alert('hello world')
-console.log('hello world');
+
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
+
+console.log(screens.toLowerCase().split(", "));
+console.log(fullPrice * (rollback / 100));
