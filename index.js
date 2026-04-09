35be982a90c53d59da81e9883d9d1b5e704aed10
Filipp <wolverinexdd@yandex.ru>
2026-04-09 14:31:40 +0400

    Lesson03

--- index.js
+++ index.js
@@ -1,19 +1,53 @@
-const title = 'Project';
-const screens = 'Простые, Сложные, Интерактивные';
-const screenPrice = 5;
+const title = prompt('Как называется ваш проект?')
+const screens = prompt('Какие типы экранов нужно разработать?');
+const screenPrice = +prompt('Сколько будет стоить данная работа');
+const adaptive = confirm('Нужен ли адаптив на сайте?');
+
+const service1 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice1 = +prompt('Сколько это будет стоить ?');
+const service2 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice2 = +prompt('Сколько это будет стоить ?');
+
 const rollback = 90;
-const fullPrice = 200000;
-const adaptive = true;
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+const servicePercentPrice = Math.ceil(fullPrice - rollback);
+
+console.log(servicePercentPrice);
+
+switch (true) {
+    case fullPrice >= 30000:
+        console.log('Даем скидку в 10%');
+        break
+
+    case fullPrice >= 15000 && fullPrice <= 30000:
+        console.log('Даем скидку в 5%');
+        break
+
+    case fullPrice <= 15000 && fullPrice >= 0:
+        console.log('Скидка не предусмотрена');
+        break
+
+    case fullPrice <= 0:
+        console.log('Что то пошло не так');
+        break
+
+
+    default:
+        break;
+}
 
 
 
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
-console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
-console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
 
-console.log(screens.toLowerCase().split(", "));
-console.log(fullPrice * (rollback / 100));
+// console.log(typeof title);
+// console.log(typeof fullPrice);
+// console.log(typeof adaptive);
+// console.log(screens.length);
+// console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+// console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
+
+// console.log(screens.toLowerCase().split(", "));
+// console.log(fullPrice * (rollback / 100));
+
